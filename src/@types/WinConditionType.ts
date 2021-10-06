c5e5592e78200bc3b1b1089e196d5dde9aecf93c
h000 <phy9250@gmail.com>
2021-10-06 17:21:21 +0900

    [FEAT] Add WinConditionType enum #5

---
+++ src/@types/WinConditionType.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+enum WinConditionType {
+  RETURN_RATE,
+  FLUCTUATION,
+  TOTAL_RETURN,
+}
+
+// const WinConditionType = {
+//   RETURN_RATE: 0,
+//   FLUCTUATION: 1,
+//   TOTAL_RETURN: 2,
+// };
+
+export default WinConditionType;
