fd7e6c2c8e3f4497ecdace6e71c3c531ce0e29c5
h000 <65332950+h000@users.noreply.github.com>
2021-10-06 19:09:40 +0900

    [FEAT] Model (#10)
    * [FEAT] Models #5
    
    - Add models (User, Room, Stock, UserStock, UserGameHistory)
    
    * [FEAT] Add WinConditionType enum #5

---
+++ src/models/Room.ts
@@ -0,0 +1,70 @@
+import { DataTypes, Model } from 'sequelize';
+import WinConditionType from '../@types/WinConditionType';
+import sequelize from './index';
+
+class Room extends Model {
+  public id!: number;
+
+  public title!: string;
+
+  public maxCapacity!: number;
+
+  public startDate!: Date;
+
+  public endDate!: Date;
+
+  public winCondition!: WinConditionType;
+
+  public invitationCode!: string;
+
+  public isEnd!: boolean;
+
+  public ownerId!: number;
+
+  public userStocks!: Array<number>;
+}
+
+Room.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    maxCapacity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    winCondition: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    invitationCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isEnd: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    userStocks: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'room',
+    createdAt: true,
+    updatedAt: false,
+  },
+);
+
+export default Room;
