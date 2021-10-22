41317b82c2149da181fb62a6f6918b6352af8272
Jiyoon Hur <60052127+jiyoon1156@users.noreply.github.com>
2021-10-22 23:37:46 +0900

    [FEAT] User Game History API (#30)
    * [FEAT] Add Finding Room to roomService #23
    
    * [FEAT] Add userGameHistory API #25
    
    * [DOCS] Update swagger accordingly #25
    
    * [DOCS] Fix typo in swagger #25
    
    - typo 'gmae' to 'game'
    
    * [CHORE] Delete unnecessary files
    
    - Delete .gitkeep, testController, testRouter
    
    * [FIX] Sequelize query #25
    
    - findOne to findAll
    - Split param
    
    * [REFACTOR] Use raw query to find game history #25
    
    - Use LEFT JOIN
    
    * [REFACTOR] Change userGameHistoryController accordingly
    - Fix userGameHistorycontroller
    - Remove unused function from roomService
    
    * [DOCS] Update Swagger accordingly #20
    
    * [FIX] request parameter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,17 @@
 import express from 'express';
 import verifyToken from '../auth/jwt';
 import roomRouter from './roomRouter';
-import testRouter from './testRouter';
+import stockRouter from './stockRouter';
+import userGameHistoryRouter from './userGameHistoryRouter';
 import userRouter from './userRouter';
 import userStockRouter from './userStockRouter';
-import stockRouter from './stockRouter';
 
 const router = express.Router();
 
-router.use('/test', testRouter);
 router.use('/user', userRouter);
 router.use('/stock', verifyToken, stockRouter);
 router.use('/room', verifyToken, roomRouter);
+router.use('/game-history', verifyToken, userGameHistoryRouter);
 router.use('/user-stock', verifyToken, userStockRouter);
 
 export default router;
