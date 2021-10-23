cb04d932cedbc6b0f894cf0fc6240ca4c4559b19
jiyoonhur <jiyoon1156@gmail.com>
2021-10-23 15:55:53 +0900

    [FIX] Add user point, avatar #31

--- src/controllers/meController.ts
+++ src/controllers/meController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
 import StatusCode from '../@types/statusCode';
+import userService from '../services/userService';
 
-const getMyInfo = (req: Request, res: Response) => {
+const getMyInfo = async (req: Request, res: Response) => {
   try {
-    const { username, userEmail } = req.decoded;
+    const { userId, username, userEmail } = req.decoded;
+    const userInfo = await userService.findUser('id', userId, ['point', 'avatar']);
 
-    return res.status(StatusCode.OK).json({ username, userEmail });
+    return res
+      .status(StatusCode.OK)
+      .json({ userId, username, userEmail, point: userInfo?.point, avatar: userInfo?.avatar });
   } catch (err) {
     return res.status(StatusCode.SERVER_ERROR).json('Error occured');
   }
