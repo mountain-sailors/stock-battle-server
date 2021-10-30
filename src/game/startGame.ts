215bf079a15504576a0f973e8830f322fa2b8e3c
h000 <phy9250@gmail.com>
2021-10-30 16:22:32 +0900

    [FIX] startGame errors #24
    - Fix wrong attribute name in Sequelize query
    - Make functions return when select query result is NULL
    - Add stock price map for current price data

--- src/game/startGame.ts
+++ src/game/startGame.ts
@@ -1,40 +1,29 @@
 import { Op, Sequelize } from 'sequelize';
 import GameStatusType from '../@types/GameStatusType';
 import Room from '../models/Room';
-import Stock from '../models/Stock';
 import UserStock from '../models/UserStock';
-
-const getInitialPrices = async () => {
-  // 가장 최근 가격이 장 시작 가격이라는 것을 보장할 수 있을지는 잘 모르겠음 -> 다음에 체크해보기
-  const stocks = await Stock.findAll({
-    attributes: ['ticker', 'price', [Sequelize.fn('max', Sequelize.col('createdAt')), 'createdAt']],
-    group: ['ticker'],
-  });
-  const stockMap = new Map<string, number>();
-  stocks.forEach((stock) => stockMap.set(stock.ticker, stock.price));
-  return stockMap;
-};
+import { stockPriceMap, updateCurrentPrices } from '../utils/stocks';
 
 const setInitialPrice = async (roomIdList: Array<number>) => {
-  const stocks = await getInitialPrices();
-  console.log(getInitialPrices);
   const userStocks = await UserStock.findAll({
     where: {
       roomId: roomIdList,
     },
   });
-  userStocks.forEach((userStock) => userStock.update({ initialPrice: stocks.get(userStock.ticker) }));
+  userStocks.forEach((userStock) => userStock.update({ initialPrice: stockPriceMap.get(userStock.ticker) }));
 };
 
 const startRoom = async (roomIdList: Array<number>, cancelled: Array<number>) => {
   const started = roomIdList.filter((t) => !cancelled.includes(t));
+  if (started.length === 0) return;
+
   await Room.update(
     {
       gameStatus: GameStatusType.IN_PROGRESS,
     },
     {
       where: {
-        roomId: started,
+        id: started,
       },
     },
   );
@@ -49,14 +38,15 @@ const cancelRoom = async (roomIdList: Array<number>) => {
       ticker: null,
     },
   });
-  const cancelledRoomIdList = cancelled.map((c) => c.id);
+  const cancelledRoomIdList = cancelled.map((c) => c.roomId);
+  if (cancelledRoomIdList.length === 0) return cancelledRoomIdList;
   await Room.update(
     {
       gameStatus: GameStatusType.CANCELLED,
     },
     {
       where: {
-        roomId: cancelledRoomIdList,
+        id: cancelledRoomIdList,
       },
     },
   );
@@ -64,14 +54,16 @@ const cancelRoom = async (roomIdList: Array<number>) => {
 };
 
 const startGame = async () => {
+  await updateCurrentPrices();
   const currentTime = new Date();
   const targets = await Room.findAll({
-    attributes: ['Id'],
+    attributes: ['id'],
     where: {
       gameStatus: GameStatusType.NOT_STARTED,
       startDate: { [Op.lte]: currentTime },
     },
   });
+  if (targets.length === 0) return;
 
   const targetIdList = targets.map((r) => r.id);
   const cancelled = await cancelRoom(targetIdList);
