6c6694e1447b918a732e0cd2f412167a939d5309
jiyoonhur <jiyoon1156@gmail.com>
2021-11-11 20:34:12 +0900

    [CHORE] Move test directory #29
    - Move out from src

--- tests/any.test.ts
+++ tests/any.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import App from '../app';
+import App from '../src/app';
 
 describe('Test the root path', () => {
   test('It should response the GET method', async () => {
