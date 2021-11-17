a2155ec027099883a7095e99729dd8d5e1825a0b
jiyoonhur <jiyoon1156@gmail.com>
2021-11-17 20:44:04 +0900

    [FIX] Error handling for user stock api #42

--- src/controllers/userStockController.ts
+++ src/controllers/userStockController.ts
@@ -1,16 +1,20 @@
 import { Request, Response } from 'express';
 import StatusCode from '../@types/statusCode';
+import { logger } from '../config/logger';
 import userStockService from '../services/userStockService';
 
 const registerStock = async (req: Request, res: Response) => {
   try {
     const { roomId, ticker, amount } = req.body;
+    if (!roomId || !ticker || !amount) return res.status(StatusCode.BAD_REQUEST).json('Invalid Input');
     const { userId } = req.decoded;
+    if (!userId) return res.status(StatusCode.SERVER_ERROR).json('Internal Server Error');
     const body = await userStockService.registerStock(roomId, ticker, amount, userId);
     return res.status(StatusCode.OK).json(body);
-  } catch (err) {
-    console.log(err);
-    return res.status(StatusCode.SERVER_ERROR).json('error');
+  } catch (err: any) {
+    logger.error(err);
+    if (err.message === 'No Such Room') return res.status(StatusCode.BAD_REQUEST).json('Invalid Input');
+    return res.status(StatusCode.SERVER_ERROR).json('Internal Server Error');
   }
 };
 
@@ -18,10 +22,11 @@ const getUserStocks = async (req: Request, res: Response) => {
   try {
     const { roomId } = req.params;
     const body = await userStockService.getUserStocks(+roomId);
+    if (body.length === 0) return res.status(StatusCode.BAD_REQUEST).json('Invalid Input');
     return res.status(StatusCode.OK).json(body);
   } catch (err) {
-    console.log(err);
-    return res.status(StatusCode.SERVER_ERROR).json('error');
+    logger.error(err);
+    return res.status(StatusCode.SERVER_ERROR).json('Internal Server Error');
   }
 };
 
