58033b5f770bab28eebcb7505eba02ed22ce4acd
h000 <phy9250@gmail.com>
2021-11-18 14:39:53 +0900

    [FIX] Use logger instead of console.error #48

--- src/controllers/userGameHistoryController.ts
+++ src/controllers/userGameHistoryController.ts
@@ -1,17 +1,17 @@
 import 'dotenv/config';
 import { Request, Response } from 'express';
 import StatusCode from '../@types/statusCode';
+import { logger } from '../config/logger';
 import userGameHistoryService from '../services/userGameHistoryService';
 
 const findGameHistory = async (req: Request, res: Response) => {
   try {
     const { userId } = req.params;
-
     const gameHistory = await userGameHistoryService.getGameHistory(+userId);
 
     return res.status(StatusCode.OK).json(gameHistory);
   } catch (error) {
-    console.error(error);
+    logger.error(error);
     return res.status(StatusCode.SERVER_ERROR).json('Internal Server Error');
   }
 };
