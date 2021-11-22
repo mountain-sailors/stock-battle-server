5dfd752caf656ddf3730184c9028ab714b9f656e
EC2 Default User <ec2-user@ip-172-31-18-177.ap-northeast-2.compute.internal>
2021-11-22 04:40:50 +0000

    [FEAT] HTTPS Setting

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import passportConfig from './auth/passport';
 import { myStream } from './config/logger';
 import initScheduler from './jobs';
 import router from './routes';
+import fs from 'fs';
+import https from 'https';
 
 const app = express();
 
@@ -14,6 +16,14 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', (req, res) => res.send('Hello Express'));
 
 app.use(cors());
+app.use(express.static('public'));
+const options = {
+  ca: fs.readFileSync('/etc/letsencrypt/live/stock-battle.p-e.kr/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/stock-battle.p-e.kr/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/stock-battle.p-e.kr/cert.pem')
+};
+//https.createServer(app).listen(3000);
+https.createServer(options, app).listen(443);
 
 // body-parser
 app.use(express.json());
