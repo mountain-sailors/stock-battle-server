efbcc1b018a852c7a859cc6f76c761763aa6f2ed
Jiyoon Hur <60052127+jiyoon1156@users.noreply.github.com>
2021-11-23 14:57:05 +0900

    [FIX] Add color to playerInfo API response & add more stock data (#58)
    * [FIX] Add color to playerInfo API response #56 #53
    
    * [CHORE] Add more stocks #56

--- src/services/playerService.ts
+++ src/services/playerService.ts
@@ -1,6 +1,8 @@
 import userService from '../services/userService';
 import userStockService from '../services/userStockService';
 
+const COLORS = ['#8B74FF', '#FF6E4F', '#54E58E', '#3063ED', '#F178B6'];
+
 const findPlayersInfo = async (roomId: number) => {
   const userStocks = await userStockService.getUserStockByRoomId(+roomId);
 
@@ -10,11 +12,12 @@ const findPlayersInfo = async (roomId: number) => {
 
   const players = await userService.findUsers('id', userIds);
   const playersInfo: object[] = [];
-  players.forEach((player) => {
+  players.forEach((player, i) => {
     playersInfo.push({
       id: player.id,
       username: player.username,
       avatar: player.avatar,
+      color: COLORS[i],
     });
   });
   return playersInfo;
