eb7075ed5f6054f56f35888d7c5d5df8cf174200
jiyoonhur <jiyoon1156@gmail.com>
2021-11-30 20:26:08 +0900

    [REFACTOR] Add new query for dashboard
    - Join stock, userStock, room table

--- src/services/dashboardService.ts
+++ src/services/dashboardService.ts
@@ -1,9 +1,44 @@
-import { Op } from 'sequelize';
+import { Op, QueryTypes } from 'sequelize';
 import getGameData from '../game/gameData';
+import sequelize from '../models';
 import Room from '../models/Room';
 import Stock from '../models/Stock';
 import UserStock from '../models/UserStock';
 import { currentStockPrices, stockArrayToObject } from '../utils/stocks';
+
+const dateFormatter = (date: Date) => {
+  const year = date.getFullYear();
+  const month = `0${date.getMonth() + 1}`.slice(-2);
+  const day = `0${date.getDate()}`.slice(-2);
+  const hours = `0${date.getHours()}`.slice(-2);
+  const minutes = `0${date.getMinutes()}`.slice(-2);
+  const seconds = `0${date.getSeconds()}`.slice(-2);
+
+  const dateString = `"${year}-${month}-${day} ${hours}:${minutes}:${seconds}"`;
+
+  return dateString;
+};
+
+const getData = async (roomId: number) => {
+  const now = new Date();
+  const currentTime = dateFormatter(now);
+  const startTime = dateFormatter(new Date(new Date().setMinutes(now.getMinutes() - 100)));
+
+  const data = await sequelize.query(
+    `SELECT s.ticker, s.price, s.createdAt,
+    us.userId, us.roomId, us.amount, us.initialPrice,
+    r.winCondition, r.maxCapacity
+    FROM stock_battle.stock AS s
+    JOIN stock_battle.user_stock AS us
+    ON us.ticker = s.ticker
+    JOIN stock_battle.room AS r
+    ON us.roomId = r.id
+    WHERE s.createdAt between ${startTime} and ${currentTime}
+    AND us.roomId=${roomId} AND r.id=${roomId};`,
+    { type: QueryTypes.SELECT },
+  );
+  return data;
+};
 
 const getInitialData = async (room: Room, userStocks: Array<UserStock>) => {
   const initialData = [];
@@ -45,6 +80,7 @@ const getCurrentData = (room: Room, userStocks: Array<UserStock>) => {
 };
 
 const dashboardService = {
+  getData,
   getInitialData,
   getCurrentData,
 };
