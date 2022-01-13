e5b4d78dcd7edf595ba0206a8b641576f9973506
Jiyoon Hur <60052127+jiyoon1156@users.noreply.github.com>
2022-01-13 20:20:15 +0900

    [hotfix] cicd permission (#73)
    * [FIX] initialize, start script #71
    
    - fix directories
    
    * [CHORE] Add comments #71
    
    * [CHORE] Change permission #71

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 echo "================kill process & run program======================="
 kill -9 `ps -ef | grep node | awk '{print $2}'`
+cp /home/ec2-user/.env .
 nohup npm run dev &
 echo "================================================================="
