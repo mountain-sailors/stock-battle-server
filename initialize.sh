d99c1dfe2f4e69e7875d647a8acddddf00b1bb28
jiyoonhur <jiyoon1156@gmail.com>
2022-01-13 13:16:25 +0900

    [CHORE] Add comments #71

--- initialize.sh
+++ initialize.sh
@@ -1,1 +1,3 @@
+echo "================initializing======================="
 if [ -d "/home/ec2-user/build" ]; then rm -Rf "/home/ec2-user/build"; fi
+echo "==================================================="
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+echo "================kill process & run program======================="
 kill -9 `ps -ef | grep node | awk '{print $2}'`
 nohup npm run dev &
+echo "================================================================="
