8d36c5c538f119467ef031a0a5cfa247171d7648
jiyoonhur <jiyoon1156@gmail.com>
2022-01-13 21:00:40 +0900

    [hotfix] application start script

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 echo "================kill process & run program======================="
+sudo chown -R ec2-user build
 kill -9 `ps -ef | grep node | awk '{print $2}'`
-cp /home/ec2-user/.env .
+rm -rf node_modules/
+npm install
+cp /home/ec2-user/.env build
 nohup npm run dev &
 echo "================================================================="
