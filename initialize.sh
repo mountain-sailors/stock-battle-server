3621b7972bb580d673a9f20d715e287251603567
jiyoonhur <jiyoon1156@gmail.com>
2022-01-31 21:09:08 +0900

    [CHORE] Remove useless files

--- initialize.sh
+++
@@ -1,8 +0,0 @@
-echo "================initializing======================="
-# if [ -d "/home/ec2-user/build" ]; then rm -Rf "/home/ec2-user/build"; fi
-sudo chown -R ec2-user build
-rm -rf node_modules/
-npm install
-FILE="/home/ec2-user/build/.env"
-if [ ! -e $FILE ]; then cp /home/ec2-user/.env /home/ec2-user/build; fi
-echo "==================================================="
--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 echo "================kill process & run program======================="
-# sudo chown -R ec2-user build
+sudo chown -R ec2-user build
 kill -9 `ps -ef | grep node | awk '{print $2}'`
-# rm -rf node_modules/
-# npm install
+rm -rf node_modules/
+npm install
 cp /home/ec2-user/.env build
 nohup npm run dev &
 echo "================================================================="
