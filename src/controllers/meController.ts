5dbd8792053386623dabc147d19eeef0459006b2
h000 <phy9250@gmail.com>
2022-07-20 20:20:33 +0900

    [FIX] Fix username bug #22

--- src/controllers/meController.ts
+++ src/controllers/meController.ts
@@ -6,11 +6,10 @@ import userService from '../services/userService';
 const getMyInfo = async (req: Request, res: Response) => {
   try {
     const { userId, username, userEmail } = req.decoded;
-    const userInfo = await userService.findUser('id', userId, ['point', 'avatar']);
-
+    const userInfo = await userService.findUser('id', userId, ['username', 'point', 'avatar']);
     return res
       .status(StatusCode.OK)
-      .json({ userId, username, userEmail, point: userInfo?.point, avatar: userInfo?.avatar });
+      .json({ userId, username: userInfo?.username, userEmail, point: userInfo?.point, avatar: userInfo?.avatar });
   } catch (err) {
     logger.error(err);
     return res.status(StatusCode.SERVER_ERROR).json('Internal Server Error');
